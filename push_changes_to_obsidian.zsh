7a219d3f59d53cd25a52c2b9ba4adeea04551015
Dan Thompson <danielrthompsonjr@gmail.com>
2023-06-13 03:07:17 -0400

    small changes to obsidian plugin

---
+++ push_changes_to_obsidian.zsh
@@ -0,0 +1,4 @@
+npm run build
+cp main.js ~/Obsidian/Synced\ Vault/.obsidian/plugins/dan-whisper/main.js
+cp manifest.json ~/Obsidian/Synced\ Vault/.obsidian/plugins/dan-whisper/manifest.json
+cp styles.css ~/Obsidian/Synced\ Vault/.obsidian/plugins/dan-whisper/styles.css
